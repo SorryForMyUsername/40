30d7d0f7b6c70ca8cc22898cc847cf7bf2f921a6
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-04-24 10:57:48 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,20 +9,25 @@ namespace Delegates_1
 {
     internal class Program
     {
-        delegate int MathOps(int a, int b);   
+        delegate int MathOps(int a, int b);
         static void Main(string[] args)
         {
+            Console.Write("M = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("N = ");
+            int b = int.Parse(Console.ReadLine());
+
             MathOps sum = Sum;
-            Console.WriteLine($"5 + 6 = {sum.Invoke(5, 6)}");
+            Console.WriteLine($"{a} + {b} = {sum.Invoke(a, b)}");
 
             MathOps sub = Sub;
-            Console.WriteLine($"5 - 6 = {sub.Invoke(5, 6)}");
+            Console.WriteLine($"{a} - {b} = {sub.Invoke(a, b)}");
 
             MathOps mul = Multiply;
-            Console.WriteLine($"5 * 6 = {mul.Invoke(5, 6)}");
+            Console.WriteLine($"{a} * {b} = {mul.Invoke(a, b)}");
 
             MathOps div = Divide;
-            Console.WriteLine($"5 / 6 = {div.Invoke(5, 6)}");
+            Console.WriteLine($"{a} / {b} = {div.Invoke(a, b)}");
 
             Console.ReadKey();
         }
