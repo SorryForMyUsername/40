6858848cb833a122cf1ac183bcd47edcb67b794a
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-04-24 10:41:15 +0300

    Add files via upload

---
+++ GoodDay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_1
+{
+    internal class GoodDay
+    {
+        public static void Output() => Console.WriteLine("Добрый день");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_1
+{
+    internal class Program
+    {
+        delegate void Message(); // 1. Объявляется делегат
+        static void Main(string[] args)
+        {
+            Message message; // 2. Создаётся объекта делегата
+            if (DateTime.Now.Hour < 12) message = GoodMorning.Output;
+            else                        message = GoodDay.Output;
+            message();
+
+            Console.ReadKey(true);
+        }
+    }
+}
