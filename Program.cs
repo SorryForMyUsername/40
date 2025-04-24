8dd8c3d4ab69a3a35cb4926f116a0c9ad956b5c1
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-04-24 10:55:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_1
+{
+    internal class Program
+    {
+        delegate double MathOps(int m, int n);
+        static void Main(string[] args)
+        {
+            MathOps mathOps = Average;
+            Console.Write("M = ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(mathOps.Invoke(m, n));
+
+            Console.ReadKey();
+        }
+
+        static double Average(int m, int n)
+        {
+            return Enumerable.Range(m, n - m + 1).Average();
+        }
+    }
+}
