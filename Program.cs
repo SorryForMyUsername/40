a9b855380c32d827ef08394d87fb369ce6bd4f00
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-04-24 19:06:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40
+{
+    internal class Program
+    {
+        delegate double MathOps(int m, int n);
+        static void Main(string[] args)
+        {
+            MathOps mathOps = Sum;
+            Operation(mathOps);
+            Operation(mathOps);
+            Operation(mathOps);
+
+            Console.ReadKey(true);
+        }
+
+        static void Operation(MathOps func)
+        {
+            Console.Write("M = ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Результат: {func.Invoke(m, n)}\n");
+        }
+
+        public static double Sum(int m, int n)
+        {
+            return Enumerable.Range(m, n - m + 1).Sum();
+        }
+    }
+}
